1347fb4b718f519b436716a7961e9cc267f7bf8b
Arezou Goshtasbi <arezoo.goshtasbi69@gmail.com>
2025-04-23 16:03:05 +0200

    css edit top scrool

--- football/static/football/js/scroll_to_top.js
+++ football/static/football/js/scroll_to_top.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const scrollBtn = document.getElementById("scrollToTopBtn");
   
-    window.onscroll = function () {
-      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
         scrollBtn.style.display = "block";
       } else {
         scrollBtn.style.display = "none";
       }
-    };
+    });
   
     scrollBtn.addEventListener("click", () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
     });
   });
   
